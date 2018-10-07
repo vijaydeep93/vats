e2dbc9e9303bfdf421554058b3e509137fd21e02
Vijaydeep Sharma <vdsharma93@gmail.com>
2018-10-07 14:45:37 +0530

    add arguments for plugin

--- commands/core/plugin/__init__.py
+++ commands/core/plugin/__init__.py
@@ -3,7 +3,19 @@ arguments = []
 
 add = (
     ('--add',),
-    {'dest': 'repo'}
+    {'dest': 'add_repo', 'help': 'Add a repo as a plugin. Requires a git link'}
+)
+
+remove = (
+    ('--rm',),
+    {'dest': 'rm_plugin', 'help': 'Removes a plugin. Requires a plugin name'}
+)
+
+list = (
+    ('--list',),
+    {'action': 'store_true', 'help': 'Lists all the extra install plugin'}
 )
 
 arguments.append(add)
+arguments.append(remove)
+arguments.append(list)
