ab93cca963d80e56f369478a2c68a494eac16049
Vijaydeep Sharma <vdsharma93@gmail.com>
2018-10-07 19:16:27 +0530

    add operations

---
+++ platform/operations.py
@@ -0,0 +1,35 @@
+"""
+File handling platform operations
+"""
+
+# python imports
+
+# third-party imports
+
+# platform imports
+from platform.io.input import Prompt
+
+# local imports
+
+class Operation(object):
+    """
+    Entry class of the program
+    """
+
+    def get_prompt(self):
+        """
+        Returns the command and args
+        """
+
+        args = Prompt().parse()
+        command = args.command
+        del args.command
+        return command, vars(args)
+
+    def execute(self):
+        """
+        Execute command
+        """
+
+        command, kwargs = self.get_prompt()
+        command.execute(**kwargs)
